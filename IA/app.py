bf21d65414823327c322d14aaccf300c5b1ec881
Vinicius Gouveia <vinicius.oliveira00@souunit.com.br>
2023-04-18 12:20:33 -0300

    configuração inicial para funcionar com remote interpreter no pycharm

--- IA/app.py
+++ IA/app.py
@@ -1,34 +1,42 @@
 import openai
-import time
+# import time
+# from dotenv import load_dotenv
+# import os
 
-# Configura a chave da API do OpenAI
-openai.api_key = "sua_chave_de_api_aqui"
+# load_dotenv()
 
-# Define o modelo GPT-3 que você deseja usar
-model_engine = "text-davinci-002"
+# # Configura a chave da API do OpenAI
+# openai.api_key = os.getenv("OPENAI_KEY")
+#
+# # Define o modelo GPT-3 que você deseja usar
+# model_engine = os.getenv("OPENAI_MODEL")
+
 
 # Define a função de geração de texto
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    time.sleep(1) # espera 1 segundo para evitar limites da API
-    message = response.choices[0].text
-    return message.strip()
-
-# Loop principal do chatbot
-while True:
-    # Obtém a entrada do usuário
-    user_input = input("Você: ")
-
-    # Gera uma resposta do modelo GPT-3
-    prompt = "Conversa: {}\nVocê:".format(user_input)
-    response = generate_text(prompt)
-
-    # Imprime a resposta do chatbot
-    print("Chatbot:", response)
+# def generate_text(prompt2):
+#     response = openai.Completion.create(
+#         engine=model_engine,
+#         prompt=prompt2,
+#         max_tokens=1024,
+#         n=1,
+#         stop=None,
+#         temperature=0.5,
+#     )
+#     time.sleep(1)  # espera 1 segundo para evitar limites da API
+#     message = response.choices[0].text
+#     return message.strip()
+#
+#
+# # Loop principal do chatbot
+# while True:
+#     # Obtém a entrada do usuário
+#     user_input = input("Você: ")
+#
+#     # Gera uma resposta do modelo GPT-3
+#     prompt = "Conversa: {}\nVocê:".format(user_input)
+#     response = generate_text(prompt)
+#
+#     # Imprime a resposta do chatbot
+#     print("Chatbot:", response)
+print("Chatbot:")
+# print(os.getenv("OPENAI_KEY"))
