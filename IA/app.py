ebd22b4f058b6d522e8faf84142416a6d401dca0
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2023-04-19 14:45:57 -0300

    openai whisper working but still need adjustments

--- IA/app.py
+++ IA/app.py
@@ -7,87 +7,42 @@ from flask import Flask, jsonify, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-load_dotenv()
-
 app = Flask(__name__)
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
 openai.organization = os.getenv("OPENAI_ORG")
 headers = {"Content-Type": "application/json", "Authorization": "Bearer " + os.getenv("OPENAI_KEY")}
 model = 'gpt-3.5-turbo'
 
+messages = []
 
-@app.route('/')
+
+@app.get('/home')
 def homepage():
-    return "IA module is running"
+    return "IA module is running", 200
 
 
-@app.route('/chat', methods=['POST'])
+@app.get('/models')
+def models():
+    return jsonify(openai.Model.list()), 200
+
+
+@app.post('/chat/completions')
 def chat_completions():
     data = request.get_json()
-    text = data.get('text')
-    response = openai.Completion.create(
-        engine=model,
-        prompt=text,
-        max_tokens=1024,
+    messages.append(data.get('message'))
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=data.get('messages'),
+        max_tokens=7,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    message = response.choices[0].text
-    return jsonify(message)
+    # message = response.choices[0].text
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
-    app.run()
-
-# GET MODEL LIST
-# response = requests.get('https://api.openai.com/v1/models', headers=headers)
-# print(response.status_code)
-# print(response.text)
-
-# GET MODEL
-# response = requests.get(f'https://api.openai.com/v1/models/{model}', headers=headers)
-# print(response.status_code)
-# print(response.text)
-
-# POST COMPLETIONS
-# payload = {'model': f'{model}', 'messages': [{'role': 'user', 'content': 'Como posso acender a luz?'}], 'temperature': 0.7}
-# response = requests.post('https://api.openai.com/v1/chat/completions', json=payload, headers=headers)
-# print(response.status_code)
-# print(response.text)
-
-
-#
-# # Define o modelo GPT-3 que você deseja usar
-# model_engine = os.getenv("OPENAI_MODEL")
-
-
-# Define a função de geração de texto
-# def generate_text(prompt2):
-#     response = openai.Completion.create(
-#         engine=model_engine,
-#         prompt=prompt2,
-#         max_tokens=1024,
-#         n=1,
-#         stop=None,
-#         temperature=0.5,
-#     )
-#     time.sleep(1)  # espera 1 segundo para evitar limites da API
-#     message = response.choices[0].text
-#     return message.strip()
-#
-#
-# # Loop principal do chatbot
-# while True:
-#     # Obtém a entrada do usuário
-#     user_input = input("Você: ")
-#
-#     # Gera uma resposta do modelo GPT-3
-#     prompt = "Conversa: {}\nVocê:".format(user_input)
-#     response = generate_text(prompt)
-#
-#     # Imprime a resposta do chatbot
-#     print("Chatbot:", response)
-# print("Chatbot:")
-# print(os.getenv("OPENAI_KEY"))
+    app.run(port=5000)
